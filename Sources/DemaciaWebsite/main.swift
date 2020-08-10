e190598e419b481490d2ffe1d27c920c29f0639b
Noam Alffasy <noahm1234@outlook.com>
2020-08-11 01:17:45 +0300

    Added custom landing page

--- Sources/DemaciaWebsite/main.swift
+++ Sources/DemaciaWebsite/main.swift
@@ -1,12 +1,14 @@
 import Foundation
 import Publish
 import Plot
+import SassPublishPlugin
 
 // This type acts as the configuration for your website.
 struct DemaciaWebsite: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
-        case posts
+        case aboutUs
+        case sponsorship
     }
     struct ItemMetadata: WebsiteItemMetadata {
         // Add any site-specific metadata that you want to use here.
@@ -16,13 +18,22 @@ struct DemaciaWebsite: Website {
     var name = "DemaciaWebsite"
     var description = "A description of DemaciaWebsite"
     var language: Language { .english }
-    var imagePath: Path? { nil }
+    var imagePath: Path? { "logos/logo.png" }
+    var favicon: Favicon? { .init(path: "favicon.png") }
 }
 
 // This will generate your website using the built-in Foundation theme:
 try DemaciaWebsite().publish(using: [
     .addMarkdownFiles(),
+    .addSlides(),
     .copyResources(),
     .generateHTML(withTheme: .demacia),
-    .generateSiteMap()
+    .generateSiteMap(),
+    .installPlugin(
+        .compileSass(
+            sassFilePath: "Sources/sass/styles.sass",
+            cssFilePath: "styles.css",
+            compressed: true
+        )
+    )
 ])
